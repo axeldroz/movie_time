37da4e647ff7d05e3201b395acb53bb4c8bfeea2
Axel Drozdzynski <axeldroz@hotmail.fr>
2019-12-05 16:50:44 +0100

    add icons on tab bar

--- src/navigation/TabNavigator.js
+++ src/navigation/TabNavigator.js
@@ -8,16 +8,39 @@ import { createAppContainer } from 'react-navigation';
 import ProfileScreen from '../screens/ProfileScreen'
 import FeedScreen from '../screens/FeedScreen'
 import ProfileStack from './ProfileStackNavigator'
+import Icon from 'react-native-vector-icons/Ionicons';
+import React from 'react';  
 
 /**
  * It's the SignedIn view
  */
 const Tabs = createBottomTabNavigator({
     Feed: {
-      screen: FeedScreen
+      screen: FeedScreen,
+      navigationOptions:{
+        tabBarLabel: "Home",
+        tabBarIcon: ({tintColor})=>(  
+            <Icon name="ios-home" color={tintColor} size={25}/>  
+        )
+      }  
+    },
+    NewMovie: {
+      screen: ProfileStack,
+      navigationOptions:{
+        tabBarLabel: "Profile",
+        tabBarIcon:({tintColor})=>(  
+            <Icon name="md-headset" color={tintColor} size={25}/>  
+        )  
+      }  
     },
     Profile: {
-        screen: ProfileStack
+        screen: ProfileStack,
+        navigationOptions:{
+          tabBarLabel: "Profile",
+          tabBarIcon:({tintColor})=>(  
+              <Icon name="ios-contact" color={tintColor} size={25}/>  
+          )  
+        }  
       },
   });
 
